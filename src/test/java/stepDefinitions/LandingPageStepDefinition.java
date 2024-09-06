330501df510480e5e3144297daa86c598c097725
Mercury0606 <mercuryzhong0606@gmail.com>
2024-09-06 11:42:04 +0800

    Move common code into GenericUtils

--- src/test/java/stepDefinitions/LandingPageStepDefinition.java
+++ src/test/java/stepDefinitions/LandingPageStepDefinition.java
@@ -29,13 +29,6 @@ public class LandingPageStepDefinition {
 
     @Given("User is on GreenCart Landing Page")
     public void user_is_on_green_cart_landing_page() throws InterruptedException {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        options.addArguments("--disable-extensions");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        System.setProperty("webdriver.chrome.driver","/Users/zhongchongyuan/Developer/WebDrivers/chromedriver");
-        testContextSetup.driver = new ChromeDriver(options);
         testContextSetup.driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
         Thread.sleep(2000);
         System.out.println(">>>User is on GreenCart Landing Page");
@@ -44,12 +37,12 @@ public class LandingPageStepDefinition {
 
     @When("User searched with shortname {string} and extracted actual name of product")
     public void user_searched_with_shortname_and_extracted_actual_name_of_product(String shortName) throws InterruptedException {
-        LandingPage landingPage = new LandingPage(testContextSetup.driver);
+        LandingPage landingPage = testContextSetup.pageObjectManager.getLandingPage();
         landingPage.searchItem(shortName);
 
         Thread.sleep(2000);
         testContextSetup.landingPageProductName = landingPage.getSearchText().split("-")[0].trim();
-        System.out.println(">>>"+landingPageProductName+" is exteacted from Home Page ");
+        System.out.println(">>>"+testContextSetup.landingPageProductName+" is exteacted from Home Page ");
     }
 
 }
