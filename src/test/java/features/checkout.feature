e3a0228a387c3faf38099441ae2c5219c317868e
Mercury0606 <mercuryzhong0606@gmail.com>
2024-09-26 20:04:03 +0800

    Added screenshot with @AfterStep tag, will take screenshots when steps are failed.

--- src/test/java/features/checkout.feature
+++ src/test/java/features/checkout.feature
@@ -5,11 +5,11 @@ Feature: Search and add to cart and  order the Products as E2E
     Given User is on GreenCart Landing Page
     When User searched with shortname <Name> and extracted actual name of product
     And Add 3 items of the selected product to cart
-     Then User proceeds to Checkout and validate the <Name> items in checkout page
+    Then User proceeds to Checkout and validate the <Name> items in checkout page
     And Verify user has ability to enter promo code and place the order
 
 
     Examples:
       | Name |
-      | Broco  |
+      | Broco---  |
 
--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -1,9 +1,18 @@
 package stepDefinitions;
 
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
+import io.cucumber.java.Scenario;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Pdf;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.w3c.dom.Text;
 import utils.TestContextSetup;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Hooks {
     TestContextSetup testContextSetup;
@@ -13,13 +22,23 @@ public class Hooks {
     }
 
     @After
-    public void AfterScenario(){
+    public void AfterScenario() throws IOException {
 
-         testContextSetup.testBase.driver.close();
+         testContextSetup.testBase.WebDriverManager().close();
         System.out.println(">>>Browser is closed");
-        testContextSetup.testBase.driver.quit();
+        testContextSetup.testBase.WebDriverManager().quit();
         System.out.println(">>>Driver  quits");
 
+    }
+    @AfterStep
+    public void AddScreenShot(Scenario scenario) throws IOException {
+        WebDriver webDriver = testContextSetup.testBase.WebDriverManager();
+        if (scenario.isFailed()){
+            File sourcePath = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
+            byte[] bytesScreenshot = FileUtils.readFileToByteArray(sourcePath);
+            scenario.attach(bytesScreenshot,"image/png", "image");
 
+            System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>AddScreenShot"+scenario.getName());
+        }
     }
 }
