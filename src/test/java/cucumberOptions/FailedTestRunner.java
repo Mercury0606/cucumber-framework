3f1ee245beca469585e6ae36006b24ddcf890195
Mercury0606 <mercuryzhong0606@gmail.com>
2024-09-27 10:03:27 +0800

    How to rerun the failed Test scenarios with cucumber plugins

--- src/test/java/cucumberOptions/FailedTestRunner.java
+++ src/test/java/cucumberOptions/FailedTestRunner.java
@@ -4,12 +4,11 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
-@CucumberOptions(features = "src/test/java/features",glue= "stepDefinitions",
-        monochrome = true,dryRun = false,tags = "@Placeholder or @OffersPage",
+@CucumberOptions(features = "@target/failed_scenarios.txt",glue= "stepDefinitions",
+        monochrome = true,dryRun = false,
         plugin = {"html:target/cucumber.html","json:target/cucumber.json",
-                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-                "rerun:target/failed_scenarios.txt"})
-public class TestNGTestRunner extends AbstractTestNGCucumberTests {
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
+public class FailedTestRunner extends AbstractTestNGCucumberTests {
     @DataProvider(parallel = true)
     @Override
     public Object[][] scenarios() {
