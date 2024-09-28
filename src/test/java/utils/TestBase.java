0aa5e389e7c4d7002d2440ef1543480b49d6c999
Mercury0606 <mercuryzhong0606@gmail.com>
2024-09-28 16:27:13 +0800

    added driver initiation for gecko driver

--- src/test/java/utils/TestBase.java
+++ src/test/java/utils/TestBase.java
@@ -4,6 +4,8 @@ package utils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -17,16 +19,28 @@ public class TestBase {
         Properties prop = new Properties();
         prop.load(fileInputStream);
         String url = prop.getProperty("url");
+        String browser_properties = prop.getProperty("browser");
+        String browser_maven = System.getProperty("browser");
+         String result = browser_maven != null ? browser_maven : browser_properties;
+
+
         if (driver == null ){
-            if (prop.getProperty("browser").equalsIgnoreCase( "chrome")){
+            if (result.equalsIgnoreCase( "chrome")){
                 System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"/src/test/resource/chromedriver");
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--remote-allow-origins=*");
+                options.addArguments("--disable-extensions");
+                options.addArguments("--no-sandbox");
+                options.addArguments("--disable-dev-shm-usage");
+                driver = new ChromeDriver(options);
             }
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--remote-allow-origins=*");
-            options.addArguments("--disable-extensions");
-            options.addArguments("--no-sandbox");
-            options.addArguments("--disable-dev-shm-usage");
-            driver = new ChromeDriver(options);
+            if (result.equalsIgnoreCase("firefox")){
+                System.setProperty("webdriver.gecko.driver",System.getProperty("user.dir")+"/src/test/resource/geckodriver");
+
+                FirefoxOptions options = new FirefoxOptions();
+                options.setAcceptInsecureCerts(true);
+                driver = new FirefoxDriver(options);
+            }
             driver.get(url);
         }
         return driver;
